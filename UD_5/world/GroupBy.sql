f12af9db74358f238c35d41e421ecd577a22e897
victoria <victoriasegovia001.alu@stemgranada.com>
2025-02-08 11:46:46 +0100

    relacion de ejercicio de groupBy

---
+++ UD_5/world/GroupBy.sql
@@ -0,0 +1,68 @@
+USE world;
+
+-- EJ55: MUESTRA LA ESPERANZA DE VIDA MEDIA POR CONTINENTE
+SELECT 
+    Continent,
+    AVG(LifeExpectancy) AS esperanza_media_continente
+FROM country
+GROUP BY Continent;
+
+-- EJ56: MUESTRA LAS REGIONES Y LA SUPERFICIE TOTAL DE CADA REGIÓN
+SELECT
+    region,
+    SUM(SurfaceArea) AS superficie_total
+FROM country
+GROUP BY region;
+
+-- EJ57: MUESTRA EL AÑO DE INDEPENDENCIA PROMEDIO DE CADA CONTINENTE 
+-- (SIN TENER EN CUENTA VALORES NULOS)
+SELECT
+    continent,
+    AVG(IndepYear) AS independencia_media
+FROM country
+GROUP BY Continent
+HAVING AVG(IndepYear) IS NOT NULL;
+
+-- EJ58: MUESTRA LOS DISTRITOS DE ESPAÑA (TABLA CITY) 
+-- Y LA POBLACIÓN TOTAL DE CADA UNO
+SELECT
+    district, 
+    SUM(population)
+from city
+GROUP BY District
+HAVING countrycode = 'ESP';
+
+-- EJ59: SUMA EL PORCENTAJE TOTAL DE TODOS LOS IDIOMAS AGRUPANDO POR PAÍS
+SELECT
+    CountryCode,
+    SUM(percentage) AS porcentaje_idiomas
+from countrylanguage
+group by countrycode;
+
+-- EJ60: MUESTRA EL NOMBRE DE LAS REGIONES CON MÁS DE 10 MILLONES DE HABITANTES
+SELECT
+    region,
+    SUM(population) AS poblacion_regiones
+FROM country
+GROUP BY Region
+HAVING sum(Population) > 10000000;
+
+-- EJ61: MUESTRA EL NOMBRE DE LAS REGIONES DE ASIA Y AMÉRICA CON MÁS 
+-- DE 100 MILLONES DE HABITANTES
+SELECT
+    region As regiones,
+    sum(Population)
+from country
+group by Region
+having SUM(population) > 100000000; 
+
+-- EJ62: MUESTRA LOS DISTRITOS DE EUROPA CON MÁS DE 3 MILLONES 
+-- DE HABITANTES (TABLA CITY)
+
+
+-- EJ63: MUESTRA UNA LISTA DE LOS CONTINENTES Y CUÁNTOS PAÍSES 
+-- HAY EN CADA CONTINENTE
+
+
+-- EJ64: MUESTRA UNA LISTA DE LAS REGIONES Y CUÁNTOS PAÍSES 
+-- HAY EN CADA REGIÓN
